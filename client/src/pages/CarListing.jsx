d22a4abbc29942b13d75c75b8dadea894552eb72
wachira <wachiraa954@gmail.com>
2024-01-23 14:03:05 +0300

    Fix api that fetches data from backend but minor issues occuring

--- client/src/pages/CarListing.jsx
+++ client/src/pages/CarListing.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import CarItem from "../components/UI/CarItem";
-import carData from "../assets/data/carData";
 
 const CarListing = () => {
+  const [carData, setCarData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCarData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/cars");
+        if (response.ok) {
+          const data = await response.json();
+          setCarData(data);
+        } else {
+          console.error("Error fetching car data. Status:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching car data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCarData();
+  }, []);
+
   return (
     <Helmet title="Cars">
       <CommonSection title="Car Listing" />
@@ -14,9 +36,9 @@ const CarListing = () => {
         <Container>
           <Row>
             <Col lg="12">
-              <div className=" d-flex align-items-center gap-3 mb-5">
-                <span className=" d-flex align-items-center gap-2">
-                  <i class="ri-sort-asc"></i> Sort By
+              <div className="d-flex align-items-center gap-3 mb-5">
+                <span className="d-flex align-items-center gap-2">
+                  <i className="ri-sort-asc"></i> Sort By
                 </span>
 
                 <select>
@@ -27,9 +49,11 @@ const CarListing = () => {
               </div>
             </Col>
 
-            {carData.map((item) => (
-              <CarItem item={item} key={item.id} />
-            ))}
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              carData.map((item) => <CarItem item={item} key={item.id} />)
+            )}
           </Row>
         </Container>
       </section>
